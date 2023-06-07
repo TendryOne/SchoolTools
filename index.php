d1818560151c875ac526dcf225679de60294db08
TendryOne <Tendryone@hotmail.com>
2023-06-08 00:36:38 +0300

    modification legere

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ShoolTools</title>
+    <link rel="shortcut icon" href="/assets/images//logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>GeniusGate</title>
 </head>
 
 <body>
-
+    <a href="/"><img src="/assets//images//logo.png" alt="" width="100px" height="80px"></a>
+    <h1>GeniusGate</h1>
+    <h2>Ouvrez les portes du génie avec l'éducation.</h2>
 </body>
 
 </html>
