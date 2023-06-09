bc4faf141bafb11300960ac3b202b1b7b84d3cc7
TendryOne <Tendryone@hotmail.com>
2023-06-09 20:28:18 +0300

    Mise en place de la page d'accueil

--- index.php
+++ index.php
@@ -5,15 +5,105 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/fontawesome-free-6.4.0-web/css/all.min.css">
     <link rel="shortcut icon" href="/assets/images//logo.png" type="image/x-icon">
     <link rel="stylesheet" href="/assets/css/style.css">
     <title>GeniusGate</title>
 </head>
 
 <body>
-    <a href="/"><img src="/assets//images//logo.png" alt="" width="100px" height="80px"></a>
-    <h1>GeniusGate</h1>
-    <h2>Ouvrez les portes du génie avec l'éducation.</h2>
+    <div class="nav-container">
+        <nav>
+            <div class="logo">
+                <a href="/"><img src="/assets//images//logo.png" alt="" width="90px" height="80px"></a>
+                <!-- <h1>GeniusGate</h1> -->
+
+            </div>
+            <ul class="nav-content">
+                <li><a href="">Accueil</a></li>
+                <li><a href="">A propos</a></li>
+                <li><a href="">Qui nous sommes?</a></li>
+            </ul>
+            <ul class="nav-connexion-container">
+                <li><a class="join" href="http://"> Se connecter <i class="fa-solid fa-right-to-bracket"></i> </a></li>
+            </ul>
+
+        </nav>
+        <div class="slogan">
+            <h2><i class="fa-solid fa-quote-left fa-fade" style="font-size: 50px;"></i> Des professeurs engagés, des étudiants inspirés - ensemble, nous grandissons <i class="fa-sharp fa-solid fa-quote-right fa-fade" style="font-size: 50px;"></i></h2>
+            <a href="">Nous rejoindre <i class="fa-sharp fa-solid fa-graduation-cap"></i></a>
+        </div>
+
+
+    </div>
+    <section>
+        <div class="community-container">
+
+            <div class="community">
+                <div class="card">
+                    <span><i class="fa-solid fa-user-tie" style="font-size: 100px; color:var(--black);"></i>
+                        <p>Professeurs</p>
+                    </span>
+                    <span class="sideback"><i class="fa-solid fa-user" style="margin: 20px;font-size: 30px;"></i><em style="color: var(--secondary-bg);">10</em> Professeurs deja inscrit</span>
+                </div>
+                <div class="card">
+                    <span> <i class="fa-solid fa-user-graduate" style="font-size: 100px;  color:var(--black);"></i>
+                        <p>Etudiants</p>
+                    </span>
+                    <span class="sideback"><i class="fa-solid fa-user" style="margin: 20px;font-size: 30px;"></i> <em style="color: var(--secondary-bg);">3000</em>Etudiants deja inscrit</span>
+                </div>
+
+                <div class="card">
+                    <span><i class="fa-solid fa-school" style="font-size: 100px;  color:var(--black);"></i>
+                        <p>Universite</p>
+                    </span>
+                    <span class="sideback"><i class="fa-solid fa-user" style="margin: 20px;font-size: 30px;"></i><em style="color: var(--secondary-bg);">2</em> Universite nous ont fait confiance</span>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="container">
+            <div class="section1">
+                <div class="image-container"></div>
+                <p style="color: black;">🙶 Ouvrez les portes du génie avec l'éducation 🙸</p>
+            </div>
+            <div class="section2">
+                <p>Rejoignez la communauté Genius Gate et élevez vos compétences à de nouveaux sommets, là où la passion de l'apprentissage rencontre la connexion entre enseignants et étudiants.</p>
+
+                <div class="join-container">
+                    <a href="">Nous Rejoindre <i class="fa-sharp fa-solid fa-graduation-cap"></i></a>
+                    <a href="">En savoir plus ... </a>
+                </div>
+
+            </div>
+        </div>
+        <div class="about-container">
+            <div class="about1">
+                <i class="fa-solid fa-clock"></i>
+                <span>Gain de temps</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis placeat reprehenderit id, enim ex, dignissimos at similique fugit voluptatibus delectus quisquam voluptates possimus cupiditate dolor nesciunt sed tempora quos explicabo?
+                    Nam saepe ea, pariatur praesentium cumque explicabo tenetur omnis velit, error fugiat sed. Tempora dolore hic enim aliquam distinctio odit, fugit fuga nesciunt, ex, ab quasi accusantium sit? Accusantium, omnis?</p>
+            </div>
+            <div class="about2">
+                <i class="fa-regular fa-calendar-days"></i>
+                <span>Emploi du temps</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis placeat reprehenderit id, enim ex, dignissimos at similique fugit voluptatibus delectus quisquam voluptates possimus cupiditate dolor nesciunt sed tempora quos explicabo?
+                    Nam saepe ea, pariatur praesentium cumque explicabo tenetur omnis velit, error fugiat sed. Tempora dolore hic enim aliquam distinctio odit, fugit fuga nesciunt, ex, ab quasi accusantium sit? Accusantium, omnis?</p>
+            </div>
+            <div class="about3">
+                <i class="fa-solid fa-people-group"></i>
+                <span>Proximite Prof-Etudiants</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis placeat reprehenderit id, enim ex, dignissimos at similique fugit voluptatibus delectus quisquam voluptates possimus cupiditate dolor nesciunt sed tempora quos explicabo?
+                    Nam saepe ea, pariatur praesentium cumque explicabo tenetur omnis velit, error fugiat sed. Tempora dolore hic enim aliquam distinctio odit, fugit fuga nesciunt, ex, ab quasi accusantium sit? Accusantium, omnis?</p>
+            </div>
+        </div>
+
+    </section>
 </body>
 
 </html>
